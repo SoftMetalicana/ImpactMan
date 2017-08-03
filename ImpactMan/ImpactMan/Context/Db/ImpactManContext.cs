7219175ff7c3b262b7dd984021142d9e943b7dab
ceci_sarpio <ceci_sarpio@abv.bg>
2017-08-03 19:52:36 +0300

    Context Update

---
+++ ImpactMan/ImpactMan/Context/Db/ImpactManContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpactMan.Context.Db
+{
+    class ImpactManContext:DbContext
+    {
+        public ImpactManContext():base("GameContext")
+        {
+            Database.SetInitializer<ImpactManContext>(new CreateDatabaseIfNotExists <ImpactManContext>());
+        }
+        DbSet<User> users { get; set; }
+    }
+}
