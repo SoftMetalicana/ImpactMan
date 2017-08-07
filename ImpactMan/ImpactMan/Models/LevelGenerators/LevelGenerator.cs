81b32b24b561e36d16522cd918f72388b67b3133
simonradev <simonradev@gmail.com>
2017-08-08 00:27:32 +0300

    Part of LevelGenerator.cs logic is implemented.

--- ImpactMan/ImpactMan/Models/LevelGenerators/LevelGenerator.cs
+++ ImpactMan/ImpactMan/Models/LevelGenerators/LevelGenerator.cs
@@ -2,6 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using ImpactMan.Constants.Levels;
+    using ImpactMan.Context;
     using ImpactMan.Interfaces.Globals;
     using ImpactMan.Interfaces.IO.Reader;
     using ImpactMan.Interfaces.Models.LevelGenerators;
@@ -46,9 +50,55 @@
             }
         }
 
+        private void CacheActivationLambda(string currentCsvKeyName)
+        {
+            Type typeToActivate = ImpactManContext.TypesByCsvKeyName[currentCsvKeyName];
+
+            ParameterExpression xParameter = Expression.Parameter(typeof(int), "x");
+            ParameterExpression yParameter = Expression.Parameter(typeof(int), "y");
+
+            ConstructorInfo ctorOfType = typeToActivate.GetConstructor(new[] { typeof(int), typeof(int) });
+
+            NewExpression newTypeExpression = Expression.New(ctorOfType, xParameter, yParameter);
+
+            Func<int, int, IConsequential> activatorLambda =
+                        Expression.Lambda<Func<int, int, IConsequential>>(newTypeExpression,
+                                                                          xParameter,
+                                                                          yParameter)
+                                                                          .Compile();
+
+            this.activationCache[currentCsvKeyName] = activatorLambda;
+        }
+
         public IList<IConsequential[]> GenerateLevel()
         {
             IList<IConsequential[]> generatedLevel = new List<IConsequential[]>();
+
+            using (this.fileReader)
+            {
+                int currentRow = 0;
+
+                string readLine;
+                while (!string.IsNullOrEmpty(readLine = this.fileReader.ReadLine()))
+                {
+                    string[] csvKeyNames = readLine.Split(LevelConstants.SeparatorSymbolsInFile);
+
+                    generatedLevel.Add(new IConsequential[csvKeyNames.Length]);
+                    for (int currentCol = 0; currentCol < csvKeyNames.Length; currentCol++)
+                    {
+                        string currentCsvKeyName = csvKeyNames[currentCol];
+                        
+                        if (!this.activationCache.ContainsKey(currentCsvKeyName))
+                        {
+                            this.CacheActivationLambda(currentCsvKeyName);
+                        }
+
+                        generatedLevel[currentRow][currentCol] = this.activationCache[currentCsvKeyName](0, 0);
+                    }
+
+                    currentRow++;
+                }
+            }
 
             return generatedLevel;
         }
