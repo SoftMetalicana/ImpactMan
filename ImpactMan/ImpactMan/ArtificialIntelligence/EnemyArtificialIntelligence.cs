7e0103ac2719b73b9eeca84752898900a5ae0177
simonradev <simonradev@gmail.com>
2017-08-17 11:56:08 +0300

    Now the enemies enter in the middle coridors.

--- ImpactMan/ImpactMan/ArtificialIntelligence/EnemyArtificialIntelligence.cs
+++ ImpactMan/ImpactMan/ArtificialIntelligence/EnemyArtificialIntelligence.cs
@@ -15,10 +15,12 @@
         private readonly Random random;
         private readonly ILevel level;
         private readonly IDictionary<IEnemy, Keys> enemiesAndCurrentDirections;
+        private readonly IDictionary<IEnemy, int> enemiesAndMovesMade;
 
         public EnemyArtificialIntelligence(ILevel level, params Keys[] directionKeys)
         {
             this.enemiesAndCurrentDirections = new Dictionary<IEnemy, Keys>();
+            this.enemiesAndMovesMade = new Dictionary<IEnemy, int>();
             this.random = new Random();
 
             this.directionKeys = directionKeys.ToList();
@@ -30,10 +32,15 @@
             foreach (IEnemy enemy in allEnemies)
             {
                 if (this.enemiesAndCurrentDirections.ContainsKey(enemy) &&
+                    this.enemiesAndMovesMade.ContainsKey(enemy) &&
+                    this.enemiesAndMovesMade[enemy] < 28 &&
                     enemy.TryUpdate(gameTime, new KeyboardState(this.enemiesAndCurrentDirections[enemy]), this.level))
                 {
+                    this.enemiesAndMovesMade[enemy]++;
                     continue;
                 }
+
+                this.enemiesAndMovesMade[enemy] = 0;
 
                 int currentKey = this.random.Next(0, 4);
                 Keys key = this.directionKeys[currentKey];
